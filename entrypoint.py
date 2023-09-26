4ca1efa4094cf6369d3bdf3139cf2f6cbeef8923
Igor Souza <igormcsouza@gmail.com>
2023-09-25 23:02:33 -0300

    Update css and add object management to the backend

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 import argparse
 import socketserver
@@ -12,9 +13,18 @@ logging.basicConfig(
 )
 
 
+todos = []
+
+
 class Application(BaseHTTPRequestHandler):
 
     TEMPLATE_DIR = 'templates'
+    CONTENT_TYPE = {
+        "text": "text/plain"
+    }
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
     def do_GET(self):
         if self.path == "/":
@@ -24,6 +34,15 @@ class Application(BaseHTTPRequestHandler):
 
             with open(os.path.join(self.TEMPLATE_DIR, "index.html"), "rb") as html:
                 self.wfile.write(html.read())
+
+        elif self.path == "/get-todos":
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+
+            self.wfile.write(json.dumps({"todos": todos}).encode('utf-8'))
+
+        ## Deals with static files and other paths related to the frontend.
 
         elif self.path.endswith("favicon.ico"):
             self.send_response(200)
@@ -49,11 +68,61 @@ class Application(BaseHTTPRequestHandler):
             with open(os.path.join(self.TEMPLATE_DIR, "css", self.path.removeprefix("/css/")), "rb") as css:
                 self.wfile.write(css.read())
 
+        ## Fallback
+
         else:
             self.send_response(404)
             self.send_header("Content-Type", "text/html")
             self.end_headers()
             self.wfile.write(b"<h1>404 - Not Found</h1>")
+
+    def do_POST(self):
+        if self.path == "/add-todo":
+            content_length = int(self.headers['Content-Length'])
+        
+            # Read the POST data from the request
+            post_data = self.rfile.read(content_length)
+            
+            try:
+                # Assuming the POST data is in JSON format
+                data = json.loads(post_data.decode('utf-8'))
+                
+                # Process the data
+                todos.append(data["todo"])
+                
+                # Send a response
+                self.send_response(201)
+                self.send_header("Content-type", self.CONTENT_TYPE["text"])
+                self.end_headers()
+                self.wfile.write(bytes("Data received and processed", "utf-8"))
+                
+            except json.JSONDecodeError as e:
+                logging.error(f"Error parsing json! {e}")
+
+                # Handle JSON parsing errors
+                self.send_response(400)
+                self.send_header("Content-type", self.CONTENT_TYPE["text"])
+                self.end_headers()
+                self.wfile.write(bytes("Invalid JSON data: " + str(e), "utf-8"))
+
+    def do_DELETE(self):
+        if self.path.startswith("/remove-todo"):
+            remove_todo = self.path.split("/")[-1].replace("%20", " ")
+
+            try:
+                todos.remove(remove_todo)
+
+                self.send_response(201)
+                self.send_header('Content-type', self.CONTENT_TYPE["text"])
+                self.end_headers()
+                self.wfile.write(bytes("Data received and processed", "utf-8"))
+            except ValueError:
+                logging.error(f"There is no todo: {remove_todo}")
+
+                self.send_response(400)
+                self.send_header('Content-type', self.CONTENT_TYPE["text"])
+                self.end_headers()
+                self.wfile.write(bytes(f"Invalid todo: {remove_todo}", "utf-8"))
         
 
 if __name__ == "__main__":
--- templates/js/index.js
+++ templates/js/index.js
@@ -9,17 +9,69 @@ new Vue({
     addTodo: function () {
       if (this.newTodo.length > 12) {
         this.inputWarning = "Max input size is 12!";
-        return ;
+        return;
       }
 
       if (this.newTodo.trim() !== "") {
-        this.todos.push(this.newTodo);
-        this.newTodo = "";
-        this.inputWarning = "";
+        // Send a POST request to the backend to add the new todo
+        fetch('/add-todo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ todo: this.newTodo }),
+        })
+        .then(response => {
+          if (response.ok) {
+            // If the POST request is successful, reset the input field and fetch the updated todos
+            this.newTodo = "";
+            this.inputWarning = "";
+            this.fetchTodos();
+          } else {
+            // Handle errors here
+            console.error('Error adding todo:', response.status);
+          }
+        })
+        .catch(error => {
+          console.error('Error adding todo:', error);
+        });
       }
     },
     removeTodo: function (index) {
-      this.todos.splice(index, 1);
+      // Send a request to the backend to remove the todo at the specified index
+      const todoToRemove = this.todos[index];
+      fetch(`/remove-todo/${todoToRemove}`, {
+        method: 'DELETE',
+      })
+      .then(response => {
+        if (response.ok) {
+          // If the DELETE request is successful, fetch the updated todos
+          this.fetchTodos();
+        } else {
+          // Handle errors here
+          console.error('Error removing todo:', response.status);
+        }
+      })
+      .catch(error => {
+        console.error('Error removing todo:', error);
+      });
     },
+    fetchTodos: function () {
+      // Send a GET request to the backend to fetch the updated list of todos
+      fetch('/get-todos')
+      .then(response => response.json())
+      .then(data => {
+        if (data.todos) {
+          this.todos = data.todos;
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching todos:', error);
+      });
+    },
+  },
+  mounted() {
+    // Fetch the initial list of todos when the component is mounted
+    this.fetchTodos();
   },
 });
